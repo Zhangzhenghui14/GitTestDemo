6f914b072e4fddec095946926ea6a5a33c939a01
zzh <3416734578@qq.com>
2022-09-22 15:06:58 +0800

    s

---
+++ Assets/Scripts/GameLunch.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Org.Yoobingo
+{
+
+	public class GameLunch : MonoBehaviour
+	{
+    	// Start is called before the first frame update
+   		void Start()
+    	{
+        	
+    	}
+
+    	// Update is called once per frame
+    	void Update()
+    	{
+        	
+    	}
+	}
+}
